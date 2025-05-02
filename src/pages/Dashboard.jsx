d4a619766f6ab93824c86ba9308ef842a28988c3
noumaged0o0 <nourmaged1822004@gmail.com>
2025-05-03 01:12:06 +0300

    dashboard edited

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div>
+    <div className="w-full h-screen flex justify-center items-center">
       <iframe
         src="https://pfpredictx-backend.onrender.com/dashboard/"
         width="100%"
-        height="530"
+        height="100%"
         style={{ border: "none" }}
         title="Dash Dashboard"
       />
